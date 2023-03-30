a0f7013e66c879104bdaac255506b8dbe2027444
matheuskarnas <matheuskarnas1@gmail.com>
2023-03-30 09:38:00 -0300

    Css adjusting

--- src/Components/CardOfCharacter/styles.ts
+++ src/Components/CardOfCharacter/styles.ts
@@ -1,21 +1,32 @@
 import styled from "styled-components";
 
 export const Wrapper = styled.div`
-	width: 250px;
 	border-radius: 10px;
 	border: solid 1px;
 	display: flex;
 	flex-direction: column;
 	align-items: center;
+	padding: 0.5rem 0;
 `;
 
 export const ImageCharacter = styled.img`
 	width: 200px;
 	height: 200px;
+	border-radius: 10px 10px 5px 5px;
 `;
 
 export const InformationCharacter = styled.main`
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	margin-left: 1rem;
+	h1 {
+		font-size: 1.15rem;
+	}
 	div {
-		font-size: 12px;
+		width: 100%;
+		font-size: 0.55rem;
+		line-height: 0.75rem;
 	}
 `;
---
+++ src/Components/CharacterList/styles.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Grid = styled.div`
+	display: grid;
+	grid-template-columns: repeat(auto-fit, 250px);
+	gap: 15px;
+`;
